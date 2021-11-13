21c5d6919a58134e470e1691465c76ebe523df9b
Seba Itokazu <sebitokazu@gmail.com>
2021-11-13 16:24:45 -0300

    Save video struct in vector

--- programs/messitributesolanacontract/src/lib.rs
+++ programs/messitributesolanacontract/src/lib.rs
@@ -14,9 +14,19 @@ pub mod messitributesolanacontract {
   }
 
   	// Another function woo!
-    pub fn add_video(ctx: Context<AddVideo>) -> ProgramResult {
+    pub fn add_video(ctx: Context<AddVideo>, video_link: String) -> ProgramResult {
       // Get a reference to the account and increment total_gifs.
       let base_account = &mut ctx.accounts.base_account;
+      let user = &mut ctx.accounts.user;
+
+      // Build the struct.
+        let item = ItemStruct {
+          video_link: video_link.to_string(),
+          user_address: *user.to_account_info().key,
+        };
+        
+      // Add it to the gif_list vector.
+      base_account.video_list.push(item);
       base_account.total_videos += 1;
       Ok(())
     }
@@ -38,10 +48,21 @@ pub struct StartStuffOff<'info> {
 pub struct AddVideo<'info> {
   #[account(mut)]
   pub base_account: Account<'info, BaseAccount>,
+  #[account(mut)]
+  pub user: Signer<'info>,
+}
+
+// Create a custom struct for us to work with.
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub video_link: String,
+    pub user_address: Pubkey,
 }
 
 // Tell Solana what we want to store on this account.
 #[account]
 pub struct BaseAccount {
     pub total_videos: u64,
+    // Attach a Vector of type ItemStruct to the account.
+    pub video_list: Vec<ItemStruct>,
 }
--- tests/messitributesolanacontract.js
+++ tests/messitributesolanacontract.js
@@ -1,21 +1,14 @@
 const anchor = require('@project-serum/anchor');
-
-// Need the system program, will talk about this soon.
 const { SystemProgram } = anchor.web3;
 
 const main = async() => {
   console.log("🚀 Starting test...")
 
-  // Create and set the provider. We set it before but we needed to update it, so that it can communicate with our frontend!
   const provider = anchor.Provider.env();
   anchor.setProvider(provider);
 
   const program = anchor.workspace.Messitributesolanacontract;
-	
-  // Create an account keypair for our program to use.
   const baseAccount = anchor.web3.Keypair.generate();
-
-  // Call start_stuff_off, pass it the params it needs!
   let tx = await program.rpc.startStuffOff({
     accounts: {
       baseAccount: baseAccount.publicKey,
@@ -24,23 +17,25 @@ const main = async() => {
     },
     signers: [baseAccount],
   });
-
   console.log("📝 Your transaction signature", tx);
 
-  // Fetch data from the account.
   let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
   console.log('👀 Video Count', account.totalVideos.toString())
 
-    // Call add_video!
-    await program.rpc.addVideo({
-      accounts: {
-        baseAccount: baseAccount.publicKey,
-      },
-    });
-    
-    // Get the account again to see what changed.
-    account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-    console.log('👀 Video Count', account.totalVideos.toString())
+  // You'll need to now pass a YT video embed link to the function! You'll also need to pass in the user submitting the video!
+  await program.rpc.addVideo("https://www.youtube.com/embed/R4-z118s-gM",{
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    },
+  });
+  
+  // Call the account.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 Video Count', account.totalVideos.toString())
+
+  // Access video_list on the account!
+  console.log('👀 Video List', account.videoList)
 }
 
 const runMain = async () => {

d856b0ab1484ff86077c0cd009bc762f9e968ae2
Seba Itokazu <sebitokazu@gmail.com>
2021-11-13 14:19:24 -0300

    Save video count in contract

--- programs/messitributesolanacontract/src/lib.rs
+++ programs/messitributesolanacontract/src/lib.rs
@@ -1,14 +1,47 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("Bs4SdFvqvYv2zndzEhNDdi1BUc9ffVjuNKpfy8Pf3P2a");
 
 #[program]
-pub mod myepicproject {
+pub mod messitributesolanacontract {
   use super::*;
   pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
-    Ok(())
+      // Get a reference to the account.
+      let base_account = &mut ctx.accounts.base_account;
+      // Initialize total_gifs.
+      base_account.total_videos = 0;
+      Ok(())
   }
+
+  	// Another function woo!
+    pub fn add_video(ctx: Context<AddVideo>) -> ProgramResult {
+      // Get a reference to the account and increment total_gifs.
+      let base_account = &mut ctx.accounts.base_account;
+      base_account.total_videos += 1;
+      Ok(())
+    }
 }
 
+// Attach certain variables to the StartStuffOff context.
 #[derive(Accounts)]
-pub struct StartStuffOff {}
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 10000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+// Specify what data you want in the AddVideo Context.
+// Getting a handle on the flow of things :)?
+#[derive(Accounts)]
+pub struct AddVideo<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_videos: u64,
+}
--- tests/messitributesolanacontract.js
+++ tests/messitributesolanacontract.js
@@ -1,13 +1,46 @@
 const anchor = require('@project-serum/anchor');
 
+// Need the system program, will talk about this soon.
+const { SystemProgram } = anchor.web3;
+
 const main = async() => {
   console.log("🚀 Starting test...")
 
-  anchor.setProvider(anchor.Provider.env());
-  const program = anchor.workspace.Myepicproject;
-  const tx = await program.rpc.startStuffOff();
+  // Create and set the provider. We set it before but we needed to update it, so that it can communicate with our frontend!
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Messitributesolanacontract;
+	
+  // Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Call start_stuff_off, pass it the params it needs!
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
 
   console.log("📝 Your transaction signature", tx);
+
+  // Fetch data from the account.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 Video Count', account.totalVideos.toString())
+
+    // Call add_video!
+    await program.rpc.addVideo({
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+    
+    // Get the account again to see what changed.
+    account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('👀 Video Count', account.totalVideos.toString())
 }
 
 const runMain = async () => {
